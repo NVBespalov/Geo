4b566fa78e5875ed2fd0a78e7cdb5805ba5c5fb4
Nikolay <nbespalov@icloud.com>
2015-05-04 15:21:31 +0500

    Categories tree view component

--- src/components/GeoList.js
+++ src/components/GeoList.js
@@ -4,26 +4,34 @@ var React = require('react/addons');
 
 require('styles/GeoList.less');
 var Tabs = require('react-simpletabs');
-
+var TreeView = require('react-treeview');
 var GeoList = React.createClass({
-  render: function () {
-    return (
-        <div className="col2">
-            <h1> Картографический сервис </h1>
-            <Tabs>
-                <Tabs.Panel title='Tab #1'>
-                    <h2>Content #1 here</h2>
-                </Tabs.Panel>
-                <Tabs.Panel title='Tab #2'>
-                    <h2>Content #2 here</h2>
-                </Tabs.Panel>
-                <Tabs.Panel title='Tab #3'>
-                    <h2>Content #3 here</h2>
-                </Tabs.Panel>
-            </Tabs>
-        </div>
-      );
-  }
+    
+    render: function () {
+        return (
+            <div className="col2">
+                <h1> Картографический сервис </h1>
+                <Tabs>
+                    <Tabs.Panel title='Категории'>
+                        {this.props.dataSource.map(function (node, i) {
+                            var label = <span className="node" > Type {i} </span>;
+                            return (
+                                <TreeView key={i} nodeLabel={label}>
+                                    {node.map(function (entry) {
+                                        return <div className="info" key={entry}>{entry}</div>;
+                                    })}
+                                </TreeView>
+                            );
+                        }, this)}
+                    </Tabs.Panel>
+                    <Tabs.Panel title='Поиск'>
+                        <h2>Content #2 here</h2>
+                    </Tabs.Panel>
+
+                </Tabs>
+            </div>
+        );
+    }
 });
 
 module.exports = GeoList;
