4798009ad7fbf932d1a66f4002af1244fe5d179d
Nick Bespalov <nbespalov@fabricadc.com>
2015-05-08 15:10:41 +0500

    store  for categories

---
+++ test/spec/stores/CategoriesStore.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('CategoriesStore', function () {
+    var store;
+
+    beforeEach(function () {
+        store = require('stores/CategoriesStore.js');
+    });
+
+    it('should be defined', function () {
+        expect(store).toBeDefined();
+    });
+});
