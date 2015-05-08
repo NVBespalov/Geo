51bb99cee7cfce5281bde164b35acc705b9ed1a4
Nikolay <nbespalov@icloud.com>
2015-05-08 23:36:19 +0500

    Revert "remove redundant comma"
    This reverts commit 350a4a1973dedcb0f535f679f1f06f2d1f5c671e.

--- src/actions/CategoriesActionCreators.js
+++ src/actions/CategoriesActionCreators.js
@@ -1,18 +1,5 @@
 'use strict';
 
-var GeoAppDispatcher = require('dispatcher/GeoAppDispatcher');
-
-var CategoriesActionCreators = {
-    /**
-     * Add one category with raw data
-     * @param {object} categoryRawData The raw data that describes category
-     */
-    addOneCategory: function (categoryRawData) {
-        GeoAppDispatcher.handleViewAction({
-            actionType: 'newCategory',
-            rawData: categoryRawData
-        });
-    }
-};
+var CategoriesActionCreators = {};
 
 module.exports = CategoriesActionCreators;
