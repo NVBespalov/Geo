642f5828eae35a2f1f8ff69c8d64e330346ff1c6
Nikolay <nbespalov@icloud.com>
2015-05-08 23:31:04 +0500

    code formatting

--- webpack.config.js
+++ webpack.config.js
@@ -9,59 +9,59 @@ var webpack = require('webpack');
 
 module.exports = {
 
-  output: {
-    filename: 'main.js',
-    publicPath: '/assets/'
-  },
+    output: {
+        filename: 'main.js',
+        publicPath: '/assets/'
+    },
 
-  cache: true,
-  debug: true,
-  devtool: false,
-  entry: [
-      'webpack/hot/only-dev-server',
-      './src/components/main.js'
-  ],
+    cache: true,
+    debug: true,
+    devtool: false,
+    entry: [
+        'webpack/hot/only-dev-server',
+        './src/components/main.js'
+    ],
 
-  stats: {
-    colors: true,
-    reasons: true
-  },
+    stats: {
+        colors: true,
+        reasons: true
+    },
 
-  resolve: {
-    extensions: ['', '.js'],
-    alias: {
-      'styles': __dirname + '/src/styles',
-      'mixins': __dirname + '/src/mixins',
-      'components': __dirname + '/src/components/',
-      'stores': __dirname + '/src/stores/',
-      'actions': __dirname + '/src/actions/'
-    }
-  },
-  module: {
-    preLoaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'jsxhint'
-    }],
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'react-hot!babel-loader'
-    }, {
-      test: /\.less/,
-      loader: 'style-loader!css-loader!less-loader'
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }, {
-      test: /\.(png|jpg)$/,
-      loader: 'url-loader?limit=8192'
-    }]
-  },
+    resolve: {
+        extensions: ['', '.js'],
+        alias: {
+            'styles': __dirname + '/src/styles',
+            'mixins': __dirname + '/src/mixins',
+            'components': __dirname + '/src/components/',
+            'stores': __dirname + '/src/stores/',
+            'actions': __dirname + '/src/actions/'
+        }
+    },
+    module: {
+        preLoaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'jsxhint'
+        }],
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'react-hot!babel-loader'
+        }, {
+            test: /\.less/,
+            loader: 'style-loader!css-loader!less-loader'
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }, {
+            test: /\.(png|jpg)$/,
+            loader: 'url-loader?limit=8192'
+        }]
+    },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
 
 };
