51c274326d4f86400e8ffb04ab9788a88a737fce
Nick Bespalov <nbespalov@fabricadc.com>
2015-05-08 15:10:06 +0500

    action for categories

---
+++ src/actions/CategoriesActionCreators.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var CategoriesActionCreators = {};
+
+module.exports = CategoriesActionCreators;
---
+++ test/spec/actions/CategoriesActionCreators.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('CategoriesActionCreators', function () {
+    var action;
+
+    beforeEach(function () {
+        action = require('actions/CategoriesActionCreators.js');
+    });
+
+    it('should be defined', function () {
+        expect(action).toBeDefined();
+    });
+});
