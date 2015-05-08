350a4a1973dedcb0f535f679f1f06f2d1f5c671e
Nikolay <nbespalov@icloud.com>
2015-05-08 23:34:50 +0500

    remove redundant comma

--- src/actions/CategoriesActionCreators.js
+++ src/actions/CategoriesActionCreators.js
@@ -1,5 +1,18 @@
 'use strict';
 
-var CategoriesActionCreators = {};
+var GeoAppDispatcher = require('dispatcher/GeoAppDispatcher');
+
+var CategoriesActionCreators = {
+    /**
+     * Add one category with raw data
+     * @param {object} categoryRawData The raw data that describes category
+     */
+    addOneCategory: function (categoryRawData) {
+        GeoAppDispatcher.handleViewAction({
+            actionType: 'newCategory',
+            rawData: categoryRawData
+        });
+    }
+};
 
 module.exports = CategoriesActionCreators;
