fc485e91f0607236c80ef454f1ade81f19a7318a
Nikolay <nbespalov@icloud.com>
2015-05-08 23:49:51 +0500

    fix deleted action alias

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
             'mixins': __dirname + '/src/mixins',
             'components': __dirname + '/src/components/',
             'stores': __dirname + '/src/stores/',
-            '': __dirname + '/src/actions/',
+            'actions': __dirname + '/src/actions/',
             'constants': __dirname + '/src/constants/'
         }
     },
