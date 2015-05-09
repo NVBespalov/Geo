29a3ceec437308f0825a030912bfaa63e06ad91c
Nikolay <nbespalov@icloud.com>
2015-05-10 02:10:06 +0500

    react on categories store changes

--- src/components/GeoApp.js
+++ src/components/GeoApp.js
@@ -1,22 +1,44 @@
 'use strict';
 
 var React = require('react/addons');
-
+var CategoriesStore = require('stores/CategoriesStore');
 require('normalize.css');
 require('../styles/main.css');
+
+/**
+ * Retrieve the current Geo data from the store
+ */
+function getGeoState() {
+    return {
+        allCategories: CategoriesStore.getAllCategories() || {}
+    };
+}
 
 var GeoList = require('components/GeoList');
 var GeoDetails = require('components/GeoDetails');
 var GeoApp = React.createClass({
+    getInitialState: function() {
+        return getGeoState();
+    },
+    componentDidMount: function() {
+        CategoriesStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+        CategoriesStore.removeChangeListener(this._onChange);
+    },
     render: function () {
         return (
         <div id="wrap">
             <h1> Картографический сервис </h1>
-            <GeoList categories={[]}/>
-            <GeoDetails />
+            <GeoList />
+            <GeoDetails categories={this.state.allCategories}/>
 
         </div>
         );
+    },
+    _onChange: function() {
+        this.setState(getGeoState());
     }
 });
 
