6d781d630fb69a8869852d413f3de83bfb1d3904
Nikolay <nbespalov@icloud.com>
2015-05-09 15:22:59 +0500

    Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react/addons');
+
+require('styles/Modal.less');
+
+var Modal = React.createClass({
+    render: function () {
+        return (
+            <div />
+        )
+    }
+});
+
+module.exports = Modal;
+
