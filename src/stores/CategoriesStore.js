f7a6781058dea7fe613fccff7f1b70db85594929
Nikolay <nbespalov@icloud.com>
2015-05-10 02:07:36 +0500

    Manage new category

--- src/stores/CategoriesStore.js
+++ src/stores/CategoriesStore.js
@@ -3,12 +3,41 @@
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
 var GeoAppDispatcher = require('../dispatcher/GeoAppDispatcher');
+var CategoriesActionsConstants = require('../constants/CategoriesActions');
+var CHANGE_EVENT = 'change';
+var LocalStoreDataProvider = require('stores/LocalStorageDataProvider');
 
-var CategoriesStore = assign({}, EventEmitter.prototype, {});
+var CategoriesStore = assign({}, EventEmitter.prototype, {
 
-CategoriesStore.dispatchToken = GeoAppDispatcher.register(function (action) {
+    emitChange: function() {
+        this.emit(CHANGE_EVENT);
+    },
 
-    switch (action.type) {
+    /**
+     * @param {function} callback
+     */
+    addChangeListener: function(callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+
+    /**
+     * @param {function} callback
+     */
+    removeChangeListener: function(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    },
+    getAllCategories: function () {
+        return LocalStoreDataProvider.read(CategoriesActionsConstants.CATEGORIES_COLLECTION);
+    }
+
+});
+
+GeoAppDispatcher.register(function (action) {
+    switch (action.actionType) {
+        case CategoriesActionsConstants.CATEGORY_CREATE:
+            LocalStoreDataProvider.addCollectionItem(CategoriesActionsConstants.CATEGORIES_COLLECTION, action.rawData.name, action.rawData);
+            CategoriesStore.emitChange();
+            break;
         default:
     }
 
