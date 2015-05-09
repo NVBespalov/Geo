fc5c1bf3f40b822235fc7e627bb0a07063021979
Nikolay <nbespalov@icloud.com>
2015-05-10 02:07:19 +0500

    Constants

---
+++ src/constants/CategoriesActions.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nickbespalov on 09.05.15.
+ */
+
+'use strict';
+
+var keyMirror = require('keymirror');
+
+module.exports = keyMirror({
+    CATEGORY_CREATE: null,
+    CATEGORIES_COLLECTION: null
+});
