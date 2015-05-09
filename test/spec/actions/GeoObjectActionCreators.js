8070362efedadf854353a202cc3f74e915ac04bf
Nikolay <nbespalov@icloud.com>
2015-05-10 04:29:38 +0500

    geo object actions creator

---
+++ test/spec/actions/GeoObjectActionCreators.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('GeoObjectActionCreators', function () {
+    var action;
+
+    beforeEach(function () {
+        action = require('actions/GeoObjectActionCreators.js');
+    });
+
+    it('should be defined', function () {
+        expect(action).toBeDefined();
+    });
+});
