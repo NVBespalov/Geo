365b2b47f39b9ec08895ad09194d291f1d644820
Nikolay <nbespalov@icloud.com>
2015-05-09 20:21:52 +0500

    modal dialog for new category

--- src/components/GeoDetails.js
+++ src/components/GeoDetails.js
@@ -2,13 +2,27 @@
 
 import React from "react/addons";
 var MapComponent = require('components/Map');
+var Modal = require('components/Modal');
 
 require('styles/GeoDetails.less');
 
 var GeoDetails = React.createClass({
+    /**
+     * Click handler for new category prompt
+     * @param event
+     */
+    handlePromptNewCategory: function (event) {
+        this.refs.addCategoryModalForm.show();
+        event.preventDefault();
+    },
+    onCancelNewCategory: function (event) {
+        this.refs.addCategoryModalForm.hide();
+        event.preventDefault();
+    },
     render: function () {
         return (
             <div className="right_col">
+                <a onClick={this.handlePromptNewCategory} href="#">Новая категория</a>
                 <div className="tabs">
                     <div className="tab">
                         <input type="radio" id="tab-map" name="tab-group-2" defaultChecked/>
@@ -28,6 +42,25 @@ var GeoDetails = React.createClass({
                         </div>
                     </div>
                 </div>
+                <Modal
+                    visible={false}
+                    closable={true}
+                    ref="addCategoryModalForm">
+                    <header>
+                        <h1>Новая категория</h1>
+                    </header>
+
+                    <form id="newCategoryForm">
+                        <p className="categoryName">
+                            <b className="label">Имя:</b>
+                            <input type="text" />
+                        </p>
+                        <p className="controls">
+                            <input type="button" value="Создать категорию" />
+                            <input type="button" value="Отмена" onClick={this.onCancelNewCategory} />
+                        </p>
+                    </form>
+                </Modal>
             </div>
         );
     }
