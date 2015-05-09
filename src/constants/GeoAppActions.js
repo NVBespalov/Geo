80f0b33fd577444174102fa84e089176c655b759
Nikolay <nbespalov@icloud.com>
2015-05-10 03:52:08 +0500

    Geo objects constants

--- src/constants/GeoAppActions.js
+++ src/constants/GeoAppActions.js
@@ -8,5 +8,7 @@ var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
     CATEGORY_CREATE: null,
-    CATEGORIES_COLLECTION: null
+    CATEGORIES_COLLECTION: null,
+    GEO_OBJECTS_COLLECTION: null,
+    GEO_OBJECTS_CREATE: null
 });
