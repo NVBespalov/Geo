8b89fe7555a037fdbfb87af2435118ed5f604057
Nikolay <nbespalov@icloud.com>
2015-05-10 02:06:41 +0500

    handle prompt - create new category

--- src/actions/CategoriesActionCreators.js
+++ src/actions/CategoriesActionCreators.js
@@ -1,5 +1,18 @@
 'use strict';
 
-var CategoriesActionCreators = {};
+var GeoAppDispatcher = require('../dispatcher/GeoAppDispatcher');
+var CategoriesActionsConstants = require('../constants/CategoriesActions');
+var CategoriesActionCreators = {
+    /**
+     * Add one category with raw data
+     * @param {object} categoryRawData The raw data that describes category
+     */
+    addOneCategory: function (categoryRawData) {
+        GeoAppDispatcher.dispatch({
+            actionType: CategoriesActionsConstants.CATEGORY_CREATE,
+            rawData: categoryRawData
+        });
+    }
+};
 
 module.exports = CategoriesActionCreators;
