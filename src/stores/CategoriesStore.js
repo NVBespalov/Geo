8d429144a01b0e93a738f92674b53ec71a842511
Nikolay <nbespalov@icloud.com>
2015-05-10 21:52:29 +0500

    remove unused var

--- src/stores/CategoriesStore.js
+++ src/stores/CategoriesStore.js
@@ -4,7 +4,6 @@ var AbstractStore = require('stores/AbstractStore');
 var assign = require('object-assign');
 var GeoAppDispatcher = require('../dispatcher/GeoAppDispatcher');
 var GeoAppActionsConstants = require('../constants/GeoAppActions');
-var CHANGE_EVENT = 'change';
 var LocalStoreDataProvider = require('stores/LocalStorageDataProvider');
 
 var CategoriesStore = assign({}, AbstractStore, {
