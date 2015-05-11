d8704614a4195de7ec828ca6a4b85f41a5bb8ef3
Nikolay <nbespalov@icloud.com>
2015-05-11 13:12:14 +0500

    delete geo objects  action constant

--- src/constants/GeoAppActions.js
+++ src/constants/GeoAppActions.js
@@ -10,5 +10,6 @@ module.exports = keyMirror({
     CATEGORY_CREATE: null,
     CATEGORIES_COLLECTION: null,
     GEO_OBJECTS_COLLECTION: null,
-    GEO_OBJECTS_CREATE: null
+    GEO_OBJECTS_CREATE: null,
+    GEO_OBJECTS_DELETE: null
 });
