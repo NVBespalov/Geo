3119072405e152baffcce58acc33871cdda07f44
Nikolay <nbespalov@icloud.com>
2015-05-11 13:16:19 +0500

    Dispatch delete Geo object action

--- src/actions/GeoObjectActionCreators.js
+++ src/actions/GeoObjectActionCreators.js
@@ -2,6 +2,7 @@
 var GeoAppDispatcher = require('../dispatcher/GeoAppDispatcher');
 var GeoAppActionsConstants = require('../constants/GeoAppActions');
 var GeoObjectActionCreators = {
+
     /**
      * Add one geo object with raw data
      * @param {object} geoObjectRawData The raw data that describes geo object
@@ -9,6 +10,17 @@ var GeoObjectActionCreators = {
     addOneObject: function (geoObjectRawData) {
         GeoAppDispatcher.dispatch({
             actionType: GeoAppActionsConstants.GEO_OBJECTS_CREATE,
+            rawData: geoObjectRawData
+        });
+    },
+
+    /**
+     * Delete geo object
+     * @param {object} geoObjectRawData
+     */
+    deleteObject: function (geoObjectRawData) {
+        GeoAppDispatcher.dispatch({
+            actionType: GeoAppActionsConstants.GEO_OBJECTS_DELETE,
             rawData: geoObjectRawData
         });
     }
